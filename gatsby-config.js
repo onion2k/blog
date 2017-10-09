317594da44480e665f61d0283d619803694d3124
Chris <chris@usablehq.com>
2017-10-09 10:54:35 +0100

    Added assets

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,5 +17,28 @@ module.exports = {
     {
       resolve: 'gatsby-transformer-remark',
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Ooer",
+        short_name: "Ooer",
+        start_url: "/",
+        background_color: "#f7f0eb",
+        theme_color: "#a2466c",
+        display: "minimal-ui",
+        icons: [
+          {
+            src: `/favicons/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
   ],
 }
